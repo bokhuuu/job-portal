e91bd480bd97e0781a5984254e083f484341719d
bokhuuu <zurabokhua@gmail.com>
2025-01-28 10:20:41 +0400

    Initialize project with Laravel Sail setup, database schema, and JobOffer model, migration, factory, and seeder.

---
+++ app/Models/JobOffer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobOffer extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobOfferFactory> */
+    use HasFactory;
+
+    public static array $experience = ['junior', 'middle', 'senior'];
+    public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gob Portal</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="text-green-600">hop hop hop</div>
+</body>
+
+</html>
